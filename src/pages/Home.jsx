e5a21009652d777d700a51ba745acd72c4249ef2
Ryan Meyer <160753132+rpmeyer3@users.noreply.github.com>
2025-06-27 12:17:48 -0400

    added home.jsx divs and options.
    theme, background, nav, content, footer

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+
+export const Home = () =>  {
+
+    return <div className="min-h-screen ">
+        
+        {/* Theme Toggler */}
+
+        {/* Background Effects */}
+
+        {/* Navbar */}
+
+        {/* Content */}
+
+
+        {/* Footer */}
+
+
+    </div>;
+}
