bd401eb4af35eb81b5a6dd50ac9d925fc29b0065
Ryan Meyer <160753132+rpmeyer3@users.noreply.github.com>
2025-07-02 10:05:47 -0400

    Add heading to AboutSection component
    Introduced a container and heading with styled 'About Me' text in the AboutSection component to begin structuring the section's content.

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export const AboutSection = () => {
-    return <section id="about" className="py-24 px-4 relative"></section>
+    return <section id="about" className="py-24 px-4 relative">
+        <div className="container mx-auto max-w-5xl">
+            <h2>
+                About <span className="text-primary">Me</span>
+            </h2>
+
+    </div>
+    </section>
 }
